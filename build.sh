997c952cefe5399e5a07a34f31261439a5863947
Peter Abbott <Peter.Abbott@akqa.com>
2014-08-24 18:56:02 +0100

    better error handling

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash  -x
 
 IMAGE_ID=$1
 LOCAL_DIR=$2
 MAPPED_DIR=$3
 BUILD_CMD=$4
 
-[ -z "$IMAGE_ID" ] && exit 1
-[ -z "$LOCAL_DIR" ] && exit 1
-[ -z "$MAPPED_DIR" ] && exit 1
-[ -z "$CMD" ] && exit 1
+[ -z "$IMAGE_ID" ] && { echo "No Docker Build Image ID"; exit 1; }
+[ -z "$LOCAL_DIR" ] && { echo "No Local Build Directory Root";exit 1; }
+[ -z "$MAPPED_DIR" ] && { echo "No Mapped Container Directory";exit 1; }
+[ -z "$BUILD_CMD" ] && { echo "No Build Command";exit 1; }
 
 LOCAL_BUILD_ROOT=$LOCAL_DIR
 if [[ "$LOCAL_DIR" != /* ]]; then
@@ -17,4 +17,4 @@ elif  [[ "$LOCAL_DIR" = "." ]]; then
   LOCAL_BUILD_ROOT=$(pwd)
 fi
 
-docker -it --rm $IMAGE_ID -v $LOCAL_BUILD_ROOT:$MAPPED_DIR -w $MAPPED_DIR $CMD
+echo docker -it --rm $IMAGE_ID -v $LOCAL_BUILD_ROOT:$MAPPED_DIR -w $MAPPED_DIR bash -c "$BUILD_CMD"
