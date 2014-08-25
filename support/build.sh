cffaa85063fb509606018dfb86c9fb7a9512e525
Peter Abbott <Peter.Abbott@akqa.com>
2014-08-25 12:35:35 +0100

    adding grunt

--- support/build.sh
+++ support/build.sh
@@ -22,7 +22,7 @@ LOCAL_HOME_DIR=~$BUILD_USER
 
 echo "Using as build root directory: $LOCAL_BUILD_ROOT"
 echo "Running build as user: $BUILD_USER"
-EXEC_CMD="docker run -a stderr -a stdout --rm -v $LOCAL_HOME_DIR:/opt/build-home -v $LOCAL_BUILD_ROOT:$MAPPED_DIR -w $MAPPED_DIR -e HOME=/opt/build-home -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) -e TARGET_WORK_DIR=$MAPPED_DIR $IMAGE_ID bash -c \"$BUILD_CMD ;RVAL=$?;chown -R \\\$HOST_UID:\\\$HOST_GID \\\$TARGET_WORK_DIR /opt/build-home\";exit $RVAL;"
+EXEC_CMD="docker run -a stderr -a stdout --rm -v $LOCAL_HOME_DIR:/opt/build-home -v $LOCAL_BUILD_ROOT:$MAPPED_DIR -w $MAPPED_DIR -e HOME=/opt/build-home -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) -e TARGET_WORK_DIR=$MAPPED_DIR $IMAGE_ID bash -c \"$BUILD_CMD ;RVAL=$?;chown -R \\\$HOST_UID:\\\$HOST_GID \\\$TARGET_WORK_DIR /opt/build-home\";exit \$RVAL;"
 echo "Running with command: $EXEC_CMD"
 eval $EXEC_CMD
 RETVAL=$?
