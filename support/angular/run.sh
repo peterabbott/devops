63e6da23f7e57fcd337be5b60865f0026599fd88
Peter Abbott <Peter.Abbott@akqa.com>
2014-08-27 12:46:30 +0100

    update to Docker build process

---
+++ support/angular/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash  
+
+BUILD_DIR=$1
+
+if [ -z "$1" ]; 
+  BUILD_DIR=/opt/build
+fi
+
+if [ ! -z "$HOST_UID" ] && [ ! -z "$HOST_GID" ]; then 
+  BUILDER_USER=builder
+
+  echo "Setting up HOST user"
+  groupadd -g $HOST_GID $BUILDER_USER 
+  useradd -U -u $HOST_UID -g $HOST_GID $BUILDER_USER 
+else 
+  BUILDER_USER=$(whoami) 
+fi 
+
+SUDO_CHECK=$(sudo -u $BUILDER_USER -n true > /dev/null 2>&1) 
+if [ $? -ne 0 ]; then
+  echo "Not able to continue to run the build as $BUILDER_USER"
+  exit 1
+fi
+echo "Build from $BUILD_DIR with user $BUILDER_USER"
+set -e #set to exit on error mode
+ 
+sudo -u $BUILDER_USER npm install
+sudo -u $BUILDER_USER bower install
+sudo -u $BUILDER_USER grunt build
+
+echo "Done build"
